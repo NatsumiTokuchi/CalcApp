abd05d278ec8b660eb6f4e72aeb62bc6c695c170
NatsumiTokuchi <elizabeth.nt77@gmail.com>
2020-07-22 12:29:57 +0900

    キーボード非表示にしたいがエラー。

--- app/src/main/java/jp/techacademy/natsumi/tokuchi/MainActivity.kt
+++ app/src/main/java/jp/techacademy/natsumi/tokuchi/MainActivity.kt
@@ -1,29 +1,35 @@
 package jp.techacademy.natsumi.tokuchi
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.view.MotionEvent
 import kotlinx.android.synthetic.main.activity_main.*
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity(), View.OnFocusChangeListener, View.OnTouchListener, {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button1.setOnClickListener(this)
-        button2.setOnClickListener(this)
-        button3.setOnClickListener(this)
-        button4.setOnClickListener(this)
+        button1.setOnTouchListener(this)
+        button2.setOnTouchListener(this)
+        button3.setOnTouchListener(this)
+        button4.setOnTouchListener(this)
+
+        editText1.onFocusChangeListener = this
+        editText2.onFocusChangeListener = this
     }
 
-    override fun onClick(v: View) {
+    override fun onTouch(v: View, event: MotionEvent): Boolean {
         if (editText1.text.toString().isEmpty() || editText1.text.toString().isEmpty()) {
             Snackbar.make(v, "何か数値を入力してください。", Snackbar.LENGTH_INDEFINITE)
-                .setAction("OK"){}
+                .setAction("OK") {}
                 .show()
         } else {
             val str1 = editText1.text.toString()
@@ -36,6 +42,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             val value4 = num1 / num2
 
             val intent = Intent(this, SecondActivity::class.java)
+
             when (v.id) {
                 R.id.button1 -> intent.putExtra("VALUE", value1)
                 R.id.button2 -> intent.putExtra("VALUE", value2)
@@ -44,5 +51,18 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             }
             startActivity(intent)
         }
+        return super.onTouchEvent(event)
+    }
+
+    override fun onFocusChange(v: View, hasFocus: Boolean) {
+        if (!hasFocus) {
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(v.windowToken, 0)
+        }
+    }
+
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        focusView.requestFocus()
+        return super.dispatchTouchEvent(ev)
     }
 }
