a63e0c62cf3569f444583e1ec1f7915a9a707a8d
NatsumiTokuchi <elizabeth.nt77@gmail.com>
2020-07-22 14:55:06 +0900

    提出用。

--- app/src/main/java/jp/techacademy/natsumi/tokuchi/MainActivity.kt
+++ app/src/main/java/jp/techacademy/natsumi/tokuchi/MainActivity.kt
@@ -1,37 +1,26 @@
 package jp.techacademy.natsumi.tokuchi
 
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
-import android.view.MotionEvent
 import kotlinx.android.synthetic.main.activity_main.*
 import android.view.View
-import android.view.inputmethod.InputMethodManager
 
-class MainActivity : AppCompatActivity(), View.OnFocusChangeListener, View.OnTouchListener, {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button1.setOnTouchListener(this)
-        button2.setOnTouchListener(this)
-        button3.setOnTouchListener(this)
-        button4.setOnTouchListener(this)
-
-        editText1.onFocusChangeListener = this
-        editText2.onFocusChangeListener = this
+        button1.setOnClickListener(this)
+        button2.setOnClickListener(this)
+        button3.setOnClickListener(this)
+        button4.setOnClickListener(this)
     }
 
-    override fun onTouch(v: View, event: MotionEvent): Boolean {
-        if (editText1.text.toString().isEmpty() || editText1.text.toString().isEmpty()) {
-            Snackbar.make(v, "何か数値を入力してください。", Snackbar.LENGTH_INDEFINITE)
-                .setAction("OK") {}
-                .show()
-        } else {
+    override fun onClick(v: View) {
+        if (editText1.text.toString().isNotEmpty() && editText2.text.toString().isNotEmpty()) {
             val str1 = editText1.text.toString()
             val str2 = editText2.text.toString()
             val num1 = str1.toDouble()
@@ -42,7 +31,6 @@ class MainActivity : AppCompatActivity(), View.OnFocusChangeListener, View.OnTou
             val value4 = num1 / num2
 
             val intent = Intent(this, SecondActivity::class.java)
-
             when (v.id) {
                 R.id.button1 -> intent.putExtra("VALUE", value1)
                 R.id.button2 -> intent.putExtra("VALUE", value2)
@@ -50,19 +38,10 @@ class MainActivity : AppCompatActivity(), View.OnFocusChangeListener, View.OnTou
                 R.id.button4 -> intent.putExtra("VALUE", value4)
             }
             startActivity(intent)
+        } else {
+            Snackbar.make(v, "何か数値を入力してください。", Snackbar.LENGTH_INDEFINITE)
+            .setAction("OK") {}
+            .show()
         }
-        return super.onTouchEvent(event)
-    }
-
-    override fun onFocusChange(v: View, hasFocus: Boolean) {
-        if (!hasFocus) {
-            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-            imm.hideSoftInputFromWindow(v.windowToken, 0)
-        }
-    }
-
-    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
-        focusView.requestFocus()
-        return super.dispatchTouchEvent(ev)
     }
 }
--- app/src/main/java/jp/techacademy/natsumi/tokuchi/SecondActivity.kt
+++ app/src/main/java/jp/techacademy/natsumi/tokuchi/SecondActivity.kt
@@ -1,18 +1,16 @@
 package jp.techacademy.natsumi.tokuchi
 
-import android.annotation.SuppressLint
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
 
-    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
 
-        var result = intent.getDoubleExtra("VALUE", 0.0)
+        val result = intent.getDoubleExtra("VALUE", 0.0)
         textView2.text = result.toString()
     }
 }
